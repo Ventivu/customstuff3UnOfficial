8c9bb3fb16c0c424ac7ffebac53b3ebf9b02b2bd
cubex2 <firebeast-breuer@web.de>
2014-01-04 19:36:04 +0100

    Created a TextField, with syntax highlighting

--- customstuff3_common/cubex2/cs3/lib/Color.java
+++ customstuff3_common/cubex2/cs3/lib/Color.java
@@ -8,4 +8,5 @@ public class Color
     public static final int DARK_GREY = 0xff4b4b4b;
     public static final int LIGHT_GREY = 0xffb6b6b6;
     public static final int YELLOW = 0xffffff00;
+    public static final int BLUE = 0xff0000ff;
 }
