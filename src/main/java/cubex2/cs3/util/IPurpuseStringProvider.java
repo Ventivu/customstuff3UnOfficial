14173bcd447ffe83e0acc2c28a8664e1ec06745c
cubex2 <firebeast-breuer@web.de>
2014-09-01 12:37:56 +0200

    Removed unnecessary stuff.

--- src/main/java/cubex2/cs3/util/IPurpuseStringProvider.java
+++ src/main/java/cubex2/cs3/util/IPurpuseStringProvider.java
@@ -1,9 +1,6 @@
 package cubex2.cs3.util;
 
-import com.sun.javafx.beans.annotations.NonNull;
-
 public interface IPurpuseStringProvider
 {
-    @NonNull
     String getStringForPurpose(StringProviderPurpose purpose);
 }
