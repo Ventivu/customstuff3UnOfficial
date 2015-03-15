8ce33aadee0fc7ee88fdc94d61fdaa8f7cd2770c
cubex2 <firebeast-breuer@web.de>
2015-03-15 18:45:17 +0100

    Increased version number.

--- src/main/java/cubex2/cs3/lib/ModInfo.java
+++ src/main/java/cubex2/cs3/lib/ModInfo.java
@@ -4,6 +4,6 @@ public class ModInfo
 {
     public static final String ID = "CustomStuff3";
     public static final String NAME = "Custom Stuff 3";
-    public static final String VERSION = "0.4.4";
+    public static final String VERSION = "0.5.0";
     public static final String DEPENDENCIES = "";
 }
