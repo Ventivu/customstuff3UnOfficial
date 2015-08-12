248a197505e9c1a1b5ba70dbf38fa46107106835
cubex2 <firebeast-breuer@web.de>
2015-08-12 14:06:10 +0200

    Removed dependency on Java 8

--- src/main/java/cubex2/cs3/util/Util.java
+++ src/main/java/cubex2/cs3/util/Util.java
@@ -8,12 +8,10 @@ import net.minecraft.world.biome.BiomeGenBase;
 import net.minecraftforge.common.util.Constants;
 
 import java.util.List;
-import java.util.function.BiConsumer;
-import java.util.function.Function;
 
 public class Util
 {
-    public static <T> void writeListToNBT(String listName, List<T> list, BiConsumer<T, NBTTagCompound> writer, NBTTagCompound nbt)
+    public static <T> void writeListToNBT(String listName, List<T> list, MyBiConsumer<T, NBTTagCompound> writer, NBTTagCompound nbt)
     {
         NBTTagList tagList = new NBTTagList();
         for (T t : list)
@@ -26,7 +24,7 @@ public class Util
         nbt.setTag(listName, tagList);
     }
 
-    public static <T> void readListFromNBT(String listName, List<T> list, Function<NBTTagCompound, T> reader, NBTTagCompound nbt)
+    public static <T> void readListFromNBT(String listName, List<T> list, MyFunction<NBTTagCompound, T> reader, NBTTagCompound nbt)
     {
         NBTTagList tagList = nbt.getTagList(listName, Constants.NBT.TAG_COMPOUND);
         for (int i = 0; i < tagList.tagCount(); i++)
@@ -36,7 +34,7 @@ public class Util
         }
     }
 
-    public static final BiConsumer<ItemStack, NBTTagCompound> NBT_ITEMSTACK_WRITER = new BiConsumer<ItemStack, NBTTagCompound>()
+    public static final MyBiConsumer<ItemStack, NBTTagCompound> NBT_ITEMSTACK_WRITER = new MyBiConsumer<ItemStack, NBTTagCompound>()
     {
         @Override
         public void accept(ItemStack itemStack, NBTTagCompound nbtTagCompound)
@@ -48,7 +46,7 @@ public class Util
         }
     };
 
-    public static final Function<NBTTagCompound, ItemStack> NBT_ITEMSTACK_READER = new Function<NBTTagCompound, ItemStack>()
+    public static final MyFunction<NBTTagCompound, ItemStack> NBT_ITEMSTACK_READER = new MyFunction<NBTTagCompound, ItemStack>()
     {
         @Override
         public ItemStack apply(NBTTagCompound nbtTagCompound)
@@ -57,7 +55,7 @@ public class Util
         }
     };
 
-    public static final BiConsumer<String, NBTTagCompound> NBT_STRING_WRITER = new BiConsumer<String, NBTTagCompound>()
+    public static final MyBiConsumer<String, NBTTagCompound> NBT_STRING_WRITER = new MyBiConsumer<String, NBTTagCompound>()
     {
         @Override
         public void accept(String string, NBTTagCompound nbtTagCompound)
@@ -69,7 +67,7 @@ public class Util
         }
     };
 
-    public static final Function<NBTTagCompound, String> NBT_STRING_READER = new Function<NBTTagCompound, String>()
+    public static final MyFunction<NBTTagCompound, String> NBT_STRING_READER = new MyFunction<NBTTagCompound, String>()
     {
         @Override
         public String apply(NBTTagCompound nbtTagCompound)
@@ -78,7 +76,7 @@ public class Util
         }
     };
 
-    public static final BiConsumer<Integer, NBTTagCompound> NBT_INTEGER_WRITER = new BiConsumer<Integer, NBTTagCompound>()
+    public static final MyBiConsumer<Integer, NBTTagCompound> NBT_INTEGER_WRITER = new MyBiConsumer<Integer, NBTTagCompound>()
     {
         @Override
         public void accept(Integer i, NBTTagCompound nbtTagCompound)
@@ -90,7 +88,7 @@ public class Util
         }
     };
 
-    public static final Function<NBTTagCompound, Integer> NBT_INTEGER_READER = new Function<NBTTagCompound, Integer>()
+    public static final MyFunction<NBTTagCompound, Integer> NBT_INTEGER_READER = new MyFunction<NBTTagCompound, Integer>()
     {
         @Override
         public Integer apply(NBTTagCompound nbtTagCompound)
@@ -99,7 +97,7 @@ public class Util
         }
     };
 
-    public static final BiConsumer<BiomeGenBase, NBTTagCompound> NBT_BIOME_WRITER = new BiConsumer<BiomeGenBase, NBTTagCompound>()
+    public static final MyBiConsumer<BiomeGenBase, NBTTagCompound> NBT_BIOME_WRITER = new MyBiConsumer<BiomeGenBase, NBTTagCompound>()
     {
         @Override
         public void accept(BiomeGenBase biome, NBTTagCompound nbtTagCompound)
@@ -111,7 +109,7 @@ public class Util
         }
     };
 
-    public static final Function<NBTTagCompound, BiomeGenBase> NBT_BIOME_READER = new Function<NBTTagCompound, BiomeGenBase>()
+    public static final MyFunction<NBTTagCompound, BiomeGenBase> NBT_BIOME_READER = new MyFunction<NBTTagCompound, BiomeGenBase>()
     {
         @Override
         public BiomeGenBase apply(NBTTagCompound nbtTagCompound)
